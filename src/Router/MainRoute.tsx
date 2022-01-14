eda4949c17b76c4058764006756163f5a6fedb6b
jiaxingdong <jiaxingdong@jd.com>
2022-01-14 13:19:20 +0800

    update handle路由为/main/*的情况

--- src/Router/MainRoute.tsx
+++ src/Router/MainRoute.tsx
@@ -3,6 +3,7 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 import routes from "./mainRoutes";
 import Layout from "@/Layout";
 import Sider from "@/page/Main/Sider";
+import NoMatch from "@/page/NoMatch";
 import type { route } from "./index";
 
 const MainRoute = () => {
@@ -39,7 +40,15 @@ const MainRoute = () => {
     <Layout>
       <Sider />
       <div style={{ flex: 1 }}>
-        <Switch>{routes.map((item: route): any => filteRoute(item))}</Switch>
+        <Switch>
+          {routes.map((item: route): any => filteRoute(item))}
+          <Route
+            path="*"
+            render={(props) => {
+              document.title = "404";
+              return <NoMatch {...props} />;
+            }}
+          /></Switch>
       </div>
     </Layout>
   );
